447c01879d0b655fe68498397bbecdbbbcb2db76
CristaCodes <73950375+crista-codes@users.noreply.github.com>
2021-02-06 08:30:19 -0500

    if else
    My first time playing with if / else statements and ruby.

---
+++ if_else.rb
@@ -0,0 +1,33 @@
+# if statement
+
+if 1 + 1 == 2 then
+    puts "1 and 1 does indeed equal 2"
+end
+
+#string
+
+my_name = 'Crista'
+if my_name == 'Crista'
+    puts "Hello Christa"
+else
+    puts "Oops, I thought your name was Crista. Sorry about that, # {my_name}!"
+end
+
+# Favorite color
+
+fav_color = 'pink'
+if (fav_color == 'red')
+    puts "Red like fire!"
+elsif (fav_color == 'orange')
+    puts "Orange like well... an orange."
+elsif (fav_color == 'yellow')
+    puts "Yellow daffodils are so pretty in the spring!"
+elsif (fav_color == 'green')
+    puts "have you been to the Emerald City in Oz?"
+elsif (fav_color == 'blue')
+    puts "Blue like the sky!"
+elsif (fav_color == 'purple')
+    puts "Purple plums are the tastiest."
+else 
+    puts "Hmm, well I don't know what that color is!"
+end
