876499ab054c2703bc6c755c9c615f4ab87aee6a
CristaCodes <73950375+crista-codes@users.noreply.github.com>
2021-02-06 08:25:02 -0500

    Ruby Challenges
    Taking a ruby challenge I wrote a script to check the weather.

---
+++ the_weather.rb
@@ -0,0 +1,14 @@
+# Check the weather
+
+fav_wether = 'Sunny'
+
+case fav_wether
+when 'Rainy'
+    puts "It's rainy."
+when 'Cloudy'
+    puts "It's cloudy."
+when 'Foggy'
+    puts "It's foggy."
+when 'Sunny'
+    puts "It's sunny."
+end
