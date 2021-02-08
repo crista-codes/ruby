7eac423d9e4888f4ddbe466c3daa4bd5b89be60e
CristaCodes <73950375+crista-codes@users.noreply.github.com>
2021-02-08 13:06:04 -0500

    While loops
    A simulation that runs the annoying younger sibling are you annoyed with me yet routine.

---
+++ first_loop.rb
@@ -0,0 +1,8 @@
+# while loops
+puts "Are you annoyed with me yet? Please answer Y/N:"
+answer = gets.chomp.downcase
+
+while (answer == "n")
+    puts "Are you annoyed with me yet? Please answer Y/N:"
+    answer = gets.chomp.downcase
+end
